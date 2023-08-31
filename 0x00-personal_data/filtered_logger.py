967004462b41cff66515f244d82b6d61f1402ef7
Donald Peters <donny9585@gmail.com>
2023-08-31 20:25:41 +0100

    Implemented get_logger()

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ from typing import List, Tuple
 
 import logging
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -34,3 +36,14 @@ def filter_datum(fields: List[str], redaction: str,
                              '{}={}{}'.format(field, redaction, separator),
                              message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """Returns a Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
