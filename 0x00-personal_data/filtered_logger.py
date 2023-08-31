5b2dd0ac8277ad3a7a170aa4e99f83fb0f2f4646
Donald Peters <donny9585@gmail.com>
2023-08-31 17:07:33 +0100

    Implemented filter_datum() function

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Filtered Logger Module"""
+import re
+
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscates a log msg using re.sub()"""
+    for field in fields:
+        if field in message:
+            message = re.sub(r"{}=.*?{}".format(field, separator),
+                             '{}={}{}'.format(field, redaction, separator),
+                             message)
+    return message
