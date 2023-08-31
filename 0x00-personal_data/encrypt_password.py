8aae71fdcbb5f28f270af7a80602a77bce6f5174
Frankenstein9585 <donny9585@gmail.com>
2023-08-31 22:39:00 +0100

    Implemented hash_password()

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,6 +5,6 @@ from typing import ByteString
 import bcrypt
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """This function hashes a password using bcrypt"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
