c41c1e67261dc62b795ccc231e644530c058e3a7
Frankenstein9585 <donny9585@gmail.com>
2023-09-04 19:09:29 +0100

    Still can't figure it out

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,13 +10,17 @@ class Auth:
     """The Auth Class"""
 
     def __init__(self):
+        """Initialize the class"""
         ...
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """This returns None for now"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """This returns None for now"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """This returns None for now"""
         return None
