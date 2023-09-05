ab9908502e975f5ec495562951ff6c682f36d473
Frankenstein9585 <donny9585@gmail.com>
2023-09-05 13:44:52 +0100

    Implemented decode_base64_authorization_header()

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """This file contains the BasicAuth class"""
+import base64
+import binascii
+
 from api.v1.auth.auth import Auth
 
 
@@ -12,7 +15,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self, authorization_header: str) \
             -> str:
-        """Returns the Bas64 part of the Authorization header"""
+        """Returns the Base64 part of the Authorization header"""
         if not authorization_header:
             return None
         if type(authorization_header) != str:
@@ -20,3 +23,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) \
+            -> str:
+        """Returns the decoded value of the Base64 string"""
+        if (not base64_authorization_header or
+                type(base64_authorization_header) != str):
+            return None
+        try:
+            return (base64.b64decode(base64_authorization_header).
+                    decode('utf-8'))
+        except binascii.Error:
+            return None
