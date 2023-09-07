2e76a032c30c5d5c1ccdfd84bec0643edee89dd8
Frankenstein9585 <donny9585@gmail.com>
2023-09-07 13:31:54 +0100

    Done task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 """This file contains the SessionAuth Class"""
+import uuid
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """The SessionAuth Class"""
+    user_id_by_session_id = dict()
 
     def __init__(self):
         """Initialize the class"""
         super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session and stores the session ID in a dictionary"""
+        if not user_id or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
