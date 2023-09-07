ed6f7be44d81f7beeb4d2bb54eb6b2fb81d056e2
Frankenstein9585 <donny9585@gmail.com>
2023-09-07 18:59:14 +0100

    Implemented "/auth_session/login"

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This file contains a routes that handle session authentication"""
+"""This file contains a routes that handle Session Authentication"""
 import os
 
 from api.v1.views import app_views
