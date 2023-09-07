0f68fb88073441257873dfa7ef321d937084f70c
Frankenstein9585 <donny9585@gmail.com>
2023-09-07 18:55:37 +0100

    Implemented current_user() method in session_auth.py

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """Handles Session Authentication"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or email == '':
+        return jsonify({'error': 'email missing'}), 400
+    if not password or password == '':
+        return jsonify({'error': 'password missing'}), 400
+
+    user = User.search({'email': email})
+    if len(user) != 0:
+        user = user[0]
+    else:
+        user = None
+    if not user:
+        return jsonify({'error': 'no user found for this email'}), 400
+    if not user.is_valid_password(password):
+        return jsonify({'error': 'wrong password'}), 401
+
+    from api.v1.app import auth
+    _my_session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(os.getenv('SESSION_NAME'), _my_session_id)
+    return response
