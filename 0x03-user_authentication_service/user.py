9f96c4f1a227396bc8c7ce28b31c8e6d28f0ee8e
Frankenstein9585 <donny9585@gmail.com>
2023-09-12 15:15:05 +0100

    Updated User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """User Model from SQLAlchemy"""
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
 class User(Base):
-    """User Model"""
+    """User class representing users table"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
