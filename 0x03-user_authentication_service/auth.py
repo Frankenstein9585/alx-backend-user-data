9262b76ba2b8756b88d4405a602e8de6fc3e33b1
Frankenstein9585 <donny9585@gmail.com>
2023-09-12 23:23:57 +0100

    Updated register_user() method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,12 +18,12 @@ class Auth:
         """Registers a User"""
         try:
             user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError('User {} already exists'.format(user.email))
         except NoResultFound:
             hashed_password = _hash_password(password)
-            new_user = self._db.add_user(email, str(hashed_password))
+            new_user = self._db.add_user(email, hashed_password)
             return new_user
-        if user:
-            raise ValueError('User {} already exists'.format(user.email))
 
 
 def _hash_password(password: str) -> bytes:
