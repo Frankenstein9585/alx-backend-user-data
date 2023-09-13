86e9c7a632c5db05052bee85ba1af468e93d0fda
Frankenstein9585 <donny9585@gmail.com>
2023-09-13 19:06:21 +0100

    Implemented /update_password route

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,7 +74,7 @@ class Auth:
         """Updates a users password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = _hash_password(password)
+            hashed_password = _hash_password(password).decode('utf-8')
             self._db.update_user(user_id=user.id,
                                  hashed_password=hashed_password,
                                  reset_token=None)
