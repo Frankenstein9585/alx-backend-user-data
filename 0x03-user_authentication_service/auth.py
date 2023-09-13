71b6d5d2a9ec741f93cee9b42673373697ee256b
Frankenstein9585 <donny9585@gmail.com>
2023-09-13 16:04:44 +0100

    Implemented Auth.get_user_from_session_id()

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,6 +42,16 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        """Finds a user based on their session_id"""
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt"""
