6e4604ecd0145daecf043129da14ae8c91f2a2c6
Frankenstein9585 <donny9585@gmail.com>
2023-09-13 18:42:45 +0100

    Implemented Auth.update_password()

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,8 +75,10 @@ class Auth:
         """Updates a users password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            user.hashed_password = str(_hash_password(password))
-            user.reset_token = None
+            hashed_password = _hash_password(password)
+            self._db.update_user(user_id=user.id,
+                                 hashed_password=hashed_password,
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
         return None
