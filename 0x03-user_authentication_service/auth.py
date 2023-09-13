cad0cfedd71dd22601d8aab0dbfca746fcb35ebd
Frankenstein9585 <donny9585@gmail.com>
2023-09-13 18:17:53 +0100

    Implemented Auth.get_reset_password_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,6 +61,16 @@ class Auth:
             return
         return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a password reset token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            user.reset_token = reset_token
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt"""
