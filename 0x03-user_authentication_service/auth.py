14290d6d6a2c63adeb99d051949ad1d4337af18e
Frankenstein9585 <donny9585@gmail.com>
2023-09-14 01:28:12 +0100

    Removed prints

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,13 +76,12 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user_id=user.id,
+                                 hashed_password=hashed_password,
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
-
-        hashed_password = _hash_password(password)
-        self._db.update_user(user_id=user.id,
-                             hashed_password=hashed_password,
-                             reset_token=None)
 
 
 def _hash_password(password: str) -> bytes:
